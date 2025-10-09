0470678d19f2ed0922ea00ed61cfc8a0d705fd7c
wahyuumaternate <retmujago@gmail.com>
2025-10-09 18:58:09 +0900

    hapus modul tidak berguna

--- Modules/Pelatihan/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Pelatihan\Http\Controllers\PelatihanController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('pelatihans', PelatihanController::class)->names('pelatihan');
-});
--- Modules/Quiz/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Quiz\Http\Controllers\QuizController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('quizzes', QuizController::class)->names('quiz');
-});
